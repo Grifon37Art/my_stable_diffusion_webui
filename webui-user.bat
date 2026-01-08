ef9b632d2ae7c702ee4f39bbe50703c9a4630eeb
gyzzo <gaetanoizzo2001@gmail.com>
2026-01-08 16:14:20 +0100

    Add customizations

--- webui-user.bat
+++ webui-user.bat
@@ -2,7 +2,7 @@
 
 set PYTHON=
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=C:\Users\gaeta\Repository\my_stable_diffusion_webui\venv
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --opt-sdp-attention --no-download-sd-model --do-not-download-clip --skip-version-check
 
 call webui.bat
